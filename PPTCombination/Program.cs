79ca8bd1c9ea95eb622e6ae8592de72a675e0434
Kafony <qizhi4b@gmail.com>
2021-10-30 21:50:42 +0800

    change to .net4.8 and add one demo presentation

--- PPTCombination/Program.cs
+++ PPTCombination/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NetOffice.OfficeApi.Enums;
+using NetOffice.PowerPointApi;
+using NetOffice.PowerPointApi.Enums;
+using NetOffice.PowerPointApi.Tools;
+using System;
+
+namespace PPTCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            var app = new Application();
+            var utils = new CommonUtils(app);
+
+            var presentation = app.Presentations.Add(MsoTriState.msoTrue);
+            presentation.Slides.Add(1, PpSlideLayout.ppLayoutClipArtAndVerticalText);
+
+            var documentFile = utils.File.Combine(Environment.CurrentDirectory, "demo", DocumentFormat.Normal);
+            presentation.SaveAs(documentFile);
+
+            app.Quit();
+            app.Dispose();
+        }
+    }
+}
